17fe648e788342af977ffbd81cdc524295d9931f
emrekizilelma <emrekizilelma00@gmail.com>
2024-12-15 12:01:22 +0300

    updated

--- 1-The-Gnu-C-Compiler-Process/main.c
+++ 1-The-Gnu-C-Compiler-Process/main.c
@@ -11,11 +11,11 @@ in C programming language with Gnu C Compiler.
 
 #include <stdio.h> // Standard I/O library for C. Like printf, scanf functions etc.
 
-int main(void) // Basically entry point of C program. When its compiling,  that's where starting to compiling.
-               // There is a should be a main function in any C projects.
-{
+int main(void)  // Basically entry point of C program. When its compiling, that's where starting to compiling.
+{               // There is a should be a main function in any C projects.
+
     printf("Hello World!\n"); // Calling a function called printf which writes string to stdin.
 
     return 0;   // main function is int type so it's must be returning any value. E
-                //return EXIT_SUCCESS; also works.
+                // return EXIT_SUCCESS; also works.
 }
